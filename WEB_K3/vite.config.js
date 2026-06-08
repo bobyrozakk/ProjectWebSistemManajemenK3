e7f3d2cf2e1b6f37b3475261dca1c954820cde90
boby <aww3992@gmail.com>
2026-06-08 17:24:01 +0700

    fix: update package.json dan vite config

--- WEB_K3/vite.config.js
+++ WEB_K3/vite.config.js
@@ -1,8 +1,5 @@
-import {
-    defineConfig
-} from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
@@ -13,11 +10,6 @@ export default defineConfig({
                 'resources/js/app.js',
             ],
             refresh: true,
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
-            ],
         }),
         tailwindcss(),
     ],
